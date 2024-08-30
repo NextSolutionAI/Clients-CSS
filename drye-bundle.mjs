4cdef08eef1ec7598b4a915c10dbb905a2d278c8
NextSolutionAI <167628609+NextSolutionAI@users.noreply.github.com>
2024-08-30 22:09:22 +0200

    Update drye-bundle.mjs

--- drye-bundle.mjs
+++ drye-bundle.mjs
@@ -53506,13 +53506,16 @@
         {
           viewBox: "0 0 24 24",
           xmlns: "http://www.w3.org/2000/svg",
-          xmlnsXlink: "http://www.w3.org/1999/xlink",
+          fill: "none",
+          stroke: "gray",
+          strokeWidth: "2",
+          strokeLinecap: "round",
+          strokeLinejoin: "round",
           ...e,
         },
         $e.createElement("path", {
-          id: "vf-close",
-          d: "M13.41,12l6.3-6.29a1,1,0,1,0-1.42-1.42L12,10.59,5.71,4.29A1,1,0,0,0,4.29,5.71L10.59,12l-6.3,6.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l6.29,6.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z",
-          fill: "#666666",
+          d: "M21 12a9 9 0 1 1-3-6.7M21 3v6h-6",
+          fillRule: "evenodd",
         })
       ),
     KK = (e) =>
